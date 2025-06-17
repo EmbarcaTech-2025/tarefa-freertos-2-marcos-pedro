c573f47639db07f496cba04db11773a2126c921f
MarcosViniciusG <marcos.vnc.gundel.silva@gmail.com>
2025-06-16 21:36:31 -0300

    chore: add semaphore lib

--- include/main.h
+++ include/main.h
@@ -3,6 +3,7 @@
 
 #include "FreeRTOS.h"
 #include "task.h"
+#include "semphr.h"
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware_manager.h"
@@ -11,5 +12,7 @@ extern ssd1306_t *disp;
 extern TaskHandle_t alarm_handle;
 extern TaskHandle_t arming_handle;
 extern TaskHandle_t noise_detector_handle;
+extern SemaphoreHandle_t xArmedSemaphore;
+extern SemaphoreHandle_t xNoiseDetectedSemaphore;
 
 #endif
