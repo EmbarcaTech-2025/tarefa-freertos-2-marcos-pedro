ed39b9983f4de47e6fc17826e472bb8ce2d6bdf1
MarcosViniciusG <marcos.vnc.gundel.silva@gmail.com>
2025-06-16 21:38:37 -0300

    feat: added noise detector and alarm task

--- src/alarm.c
+++ src/alarm.c
@@ -1,7 +1,33 @@
 #include "alarm.h"
 
 void vTaskAlarm(void * pvParameters) {
+    xSemaphoreTake(xNoiseDetectedSemaphore, portMAX_DELAY);
+
     for(;;) {
-        
+
+        uint slice_num_a = pwm_gpio_to_slice_num(BUZZER_A_PIN);
+        uint channel_num_a = pwm_gpio_to_channel(BUZZER_A_PIN);
+
+        uint slice_num_b = pwm_gpio_to_slice_num(BUZZER_B_PIN);
+        uint channel_num_b = pwm_gpio_to_channel(BUZZER_B_PIN);
+
+        npClear();
+        for(uint8_t i=0; i<MATRIX_LED_COUNT; i++)
+            npSetLED(i, 1, 0, 0);
+        npWrite();
+        pwm_set_chan_level(slice_num_a, channel_num_a, 255);
+        pwm_set_chan_level(slice_num_b, channel_num_b, 255);
+        ssd1306_clear(disp);
+        ssd1306_draw_string(disp, 8, 32, 1, "LOUD NOISE DETECTED!");
+        ssd1306_show(disp);
+        vTaskDelay(pdMS_TO_TICKS(100));
+        npClear();
+        npWrite();
+        pwm_set_chan_level(slice_num_a, channel_num_a, 0);
+        pwm_set_chan_level(slice_num_b, channel_num_b, 0);
+        ssd1306_clear(disp);
+        ssd1306_show(disp);
+
+        vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
--- src/noise_detector.c
+++ src/noise_detector.c
@@ -1,7 +1,42 @@
 #include "noise_detector.h"
 
-void vTaskNoiseDetector(void * pvParameters) {
-    for(;;) {
+uint16_t adc_buffer[SAMPLES];
 
-    }
+float mic_power()
+{
+  float avg = 0.f;
+
+  for (uint i = 0; i < SAMPLES; ++i) {
+    float temp = adc_buffer[i] - DC_OFFSET;
+    avg += temp * temp;
+  }
+
+  avg /= SAMPLES;
+  return sqrt(avg);
+}
+
+void vTaskNoiseDetector(void *pvParameters)
+{
+  ssd1306_clear(disp);
+  ssd1306_draw_string(disp, 0, 32, 1, "DON'T MAKE LOUD NOISES");
+  ssd1306_show(disp);
+
+  for (;;)
+  {
+    adc_gpio_init(MIC_PIN);
+    adc_select_input(MIC_CHANNEL);
+
+    for (int i = 0; i < SAMPLES; i++)
+      adc_buffer[i] = adc_read();
+
+    float avg = mic_power();   
+    npClear(); 
+    if(avg >= SENSITIVITY_THRESHOLD) {
+      xSemaphoreGive(xNoiseDetectedSemaphore);
+      vTaskSuspend(NULL);
+    }     
+    npWrite();
+
+    vTaskDelay(pdMS_TO_TICKS(10));
+  }
 }
