c832e796a7143d5b1aa3da90df8adcf1dac29edf
Pedro Sanchez <pedrotms21@icloud.com>
2025-06-17 14:28:12 -0300

    Feature de Disarmed/Arming/Armed implementada <3

--- include/main.h
+++ include/main.h
@@ -4,6 +4,7 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include <stdio.h>
+#include <string.h>
 #include "pico/stdlib.h"
 #include "hardware_manager.h"
 
@@ -11,5 +12,8 @@ extern ssd1306_t *disp;
 extern TaskHandle_t alarm_handle;
 extern TaskHandle_t arming_handle;
 extern TaskHandle_t noise_detector_handle;
+
+extern int direction_arr[8];
+extern int answer_arr[8];
 
 #endif
